d817c5c72769782ccd8c6f3553f71d629a6a9d8b
Anshuman Chhapolia <achhap.10.01@gmail.com>
2021-02-22 21:16:47 +0530

    feat: add api implementation
    Signed-off-by: Anshuman Chhapolia <achhap.10.01@gmail.com>

--- src/components/GeneralQuestions/QuestionForm/QuestionForm.js
+++ src/components/GeneralQuestions/QuestionForm/QuestionForm.js
@@ -1,19 +1,46 @@
 import { useState } from "react";
+import { createQA } from "../../../utils/qa";
 import TextInput from "../../FormInputs/TextInput/TextInput";
 import "./QuestionForm.css";
 
 const QuestionForm = ({ gridArea = "form" }) => {
   const [data, setData] = useState({
-    question: "",
+    statement: "",
     answer: "",
-    keywords: [],
+    keywords: ''
   });
+  const updateValue = (fieldName) => (e) => {
+    const newState = { ...data }
+    newState[fieldName] = e.target.value
+    setData(newState)
+  }
+
+
+  const handleAddClick = (e) => {
+    e.preventDefault()
+    const keywords = (data.keywords ?? '').split(/ ?, ?/)
+    const response = createQA({...data, keywords})
+    if (response) {
+      // Handle Success
+    } else {
+      // Handle Failure
+    }
+  }
+
   return (
     <form className="form" style={{ gridArea }}>
-      <TextInput label="Question" dark name="question" />
-      <TextInput label="Answer" dark name="answer" />
-      <TextInput label="Keywords" dark name="keywords" />
-      <button className="dark btn submit" type="submit">
+      <TextInput
+        value={data.statement} onChange={updateValue('statement')} label="Question" dark name="question"
+      />
+      <TextInput 
+        value={data.answer} onChange={updateValue('answer')} label="Answer" dark name="answer" 
+      />
+      <TextInput 
+        value={data.keywords} onChange={updateValue('keywords')} label="Keywords" dark name="keywords" 
+      />
+      <button 
+      onClick={handleAddClick}
+      className="dark btn submit" type="submit">
         ADD
       </button>
     </form>
---
+++ src/utils/qa.js
@@ -0,0 +1,54 @@
+const stateURL = `${process.env.REACT_APP_ROOT_URL}/qa`;
+
+export const createQA = async (qa) => {
+  const body = { qa }
+  try {
+
+    const response = await window.fetch(`${stateURL}/create`, {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+        'content-type': 'application/json'
+      }
+    })
+
+    if (response.status === 201) {
+      return true
+    } else {
+      return false
+    }
+  } catch (error) {
+    console.error('Error in creating qa', { qa, body })
+    return false
+  }
+}
+
+export const readAllQa = async () => {
+  try {
+    const response = await window.fetch(`${stateURL}/readAll`)
+    if (response.status === 200) {
+      const body = await response.json()
+      return body.qas ?? []
+    } else {
+      return []
+    }
+  } catch (error) {
+    console.error('Error in fetching all data')
+    return []
+  }
+}
+
+export const deleteQa = async (id) => {
+  try {
+    const response = await window.fetch(`${stateURL}/delete?id=${id}`)
+    if (response.status === 200) {
+      return true
+    } else {
+      return false
+    }
+  } catch (error) {
+    console.error('Error in deleting QA', { id })
+    return true
+  }
+}
+
