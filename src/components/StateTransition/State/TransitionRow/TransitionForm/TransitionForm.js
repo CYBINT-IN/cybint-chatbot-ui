8f0ef6d2df40e59517dcf19b30bb1068634f3da7
Anshuman Chhapolia <achhap.10.01@gmail.com>
2021-02-25 17:53:50 +0530

    fix: transition state selection
    Signed-off-by: Anshuman Chhapolia <achhap.10.01@gmail.com>

--- src/components/StateTransition/State/TransitionRow/TransitionForm/TransitionForm.js
+++ src/components/StateTransition/State/TransitionRow/TransitionForm/TransitionForm.js
@@ -4,13 +4,15 @@ import CheckBoxInput from "../../../../FormInputs/CheckBoxInput/CheckBoxInput";
 import SelectInput from "../../../../FormInputs/SelectInput/SelectInput";
 import TextInput from "../../../../FormInputs/TextInput/TextInput";
 import { useStateTrans } from "../../../../../contexts/stateTransitionContext";
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
 import "./TransitionForm.css";
 import { ToastsStore } from "react-toasts";
 
 const initiailData = {
   message: "",
   keywords: [],
-  end: "",
+  end: false,
   spareContent: "",
   intent: "",
   transitionState: "",
@@ -27,6 +29,10 @@ const TransitionForm = ({ state, transitionIndex, closeFunc }) => {
   const [data, setData] = useState({});
   const [stateOptions, setStateOptions] = useState();
   const [isEnd, setIsEnd] = useState(false);
+  const [alertOpen, setAlertOpen] = useState(false)
+  const Alert = (props) => {
+    return <MuiAlert elevation={6} variant='filled' {...props} />;
+  };
   useEffect(() => {
     const transition = { ...state.transitions[transitionIndex] };
     if (transition.keywords) {
@@ -38,12 +44,17 @@ const TransitionForm = ({ state, transitionIndex, closeFunc }) => {
     setData(transition);
   }, [transitionIndex]);
 
+  const handleClose = () => {
+    setAlertOpen(false)
+  }
+
   useEffect(() => {
     if (stateTransData) {
-      const opts = stateTransData.map((st, i) => {
+      let opts = stateTransData.map((st, i) => {
         return { value: st._id, label: `State ${i + 1}` };
       });
-      setStateOptions(opts);
+      const newOpts = opts.filter((a) => a.value !== state._id)
+      setStateOptions(newOpts);
     }
   }, [stateTransData]);
 
@@ -65,10 +76,15 @@ const TransitionForm = ({ state, transitionIndex, closeFunc }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const submittedData = { ...data };
+    console.log(submittedData.end, submittedData.state === '')
+    if (!submittedData.end && submittedData.state === '') {
+      setAlertOpen(true)
+      return
+    }
     submittedData.keywords = getKeywords(submittedData.keywords);
     if (
       (submittedData.state === "None",
-      submittedData.state && submittedData.state.length === 0)
+        submittedData.state && submittedData.state.length === 0)
     ) {
       delete submittedData.state;
     }
@@ -84,78 +100,95 @@ const TransitionForm = ({ state, transitionIndex, closeFunc }) => {
   };
 
   return (
-    <form className="transition-form" onSubmit={handleSubmit}>
-      <TextInput
-        label="Statement"
-        name="statement"
-        value={data && data.statement}
-        onChange={(e) =>
-          setData((data) => ({ ...data, statement: e.target.value }))
-        }
-      />
-      <TextInput
-        label="Keywords"
-        name="keywords"
-        value={data && data.keywords}
-        onChange={(e) => {
-          setData((data) => ({ ...data, keywords: e.target.value }));
-        }}
-      />
-      <CheckBoxInput
-        label="End"
-        name="end"
-        disabled={true}
-        onChange={(end) => setData({ ...data, end })}
-        value={data && data.end}
-      />
-      <TextInput
-        label="Spare Content"
-        name="spare-content"
-        onChange={(e) => {
-          setData((data) => ({ ...data, spareContent: e.target.value }));
-        }}
-        value={data && data.spareContent}
-      />
-      <TextInput
-        label="Intent"
-        name="intent"
-        onChange={(e) => {
-          setData((data) => ({ ...data, intent: e.target.value }));
-        }}
-        value={data && data.intent}
-      />
-      <SelectInput
-        label="Transition State"
-        name="transition-state"
-        options={stateOptions}
-        value={data && data.state}
-        disabled={isEnd}
-        onChange={(e) => {
-          setData((data) => ({ ...data, state: e.target.value }));
-        }}
-      />
-      <TextInput
-        label="Type"
-        name="type"
-        value={data && data.type}
-        onChange={(e) => setData((data) => ({ ...data, type: e.target.value }))}
-      />
-      <div className="btn-container">
-        <button type="submit" className="dark btn">
-          ADD
-        </button>
-        <input
-          type="button"
-          onClick={async () => {
-            await deleteTransInState(state._id, transitionIndex);
-            ToastsStore.error("Deleted Transition Successfully");
-            closeFunc();
+    <>
+      <Snackbar
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        open={alertOpen}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        key='topcenter'
+      >
+        <Alert onClose={handleClose} severity='warning'>
+          Please Choose a state or mark end as true.
+        </Alert>
+      </Snackbar>
+      <form className="transition-form" onSubmit={handleSubmit}>
+        <TextInput
+          label="Statement"
+          name="statement"
+          value={data && data.statement}
+          onChange={(e) =>
+            setData((data) => ({ ...data, statement: e.target.value }))
+          }
+        />
+        <TextInput
+          label="Keywords"
+          name="keywords"
+          value={data && data.keywords}
+          onChange={(e) => {
+            setData((data) => ({ ...data, keywords: e.target.value }));
           }}
-          className="delete btn"
-          value="Delete"
-        ></input>
-      </div>
-    </form>
+        />
+        <CheckBoxInput
+          label="End"
+          name="end"
+          disabled={false}
+          onChange={(end) => setData({ ...data, end })}
+          value={data && data.end}
+        />
+        <TextInput
+          label="Spare Content"
+          name="spare-content"
+          onChange={(e) => {
+            setData((data) => ({ ...data, spareContent: e.target.value }));
+          }}
+          value={data && data.spareContent}
+        />
+        <TextInput
+          label="Intent"
+          name="intent"
+          onChange={(e) => {
+            setData((data) => ({ ...data, intent: e.target.value }));
+          }}
+          value={data && data.intent}
+        />
+        <SelectInput
+          label="Transition State"
+          name="transition-state"
+          options={stateOptions}
+          value={data && data.state}
+          // disabled={isEnd}
+          onChange={(e) => {
+            if (e.target.value !== '') {
+              setData((data) => ({ ...data, end: false, state: e.target.value }));
+            } else {
+              setData((data) => ({ ...data, end: true, state: e.target.value }));
+            }
+          }}
+        />
+        <TextInput
+          label="Type"
+          name="type"
+          value={data && data.type}
+          onChange={(e) => setData((data) => ({ ...data, type: e.target.value }))}
+        />
+        <div className="btn-container">
+          <button type="submit" className="dark btn">
+            Save
+          </button>
+          <input
+            type="button"
+            onClick={async () => {
+              await deleteTransInState(state._id, transitionIndex);
+              ToastsStore.error("Deleted Transition Successfully");
+              closeFunc();
+            }}
+            className="delete btn"
+            value="Delete"
+          ></input>
+        </div>
+      </form>
+    </>
   );
 };
 
